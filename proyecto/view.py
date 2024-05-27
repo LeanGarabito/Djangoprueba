0f05ddb06927d92b8829e8a3507114ed33bd459f
Leandro <leandro.garabito@hotmail.com>
2024-05-27 13:17:57 -0300

    creamos el template

--- proyecto/view.py
+++ proyecto/view.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse 
 from datetime import datetime
-
+from django.template import context, Template
 
 
 
@@ -15,6 +15,22 @@ def saludo2(request):
 def Dia(request):
     dia = datetime.now()
     
-    archivoTexto = f"el dia de hoy es:<br> {dia}"
+    archivoTexto = f"el dia de hoy es: {dia}"
     
     return HttpResponse(archivoTexto)
+
+def probandoElTemplate(self):
+    miHtml1 = open("C:\Users\jigwo\Desktop\Estudios\DjandoPrueba\proyecto\plantillas\template1.html")
+    
+    plantilla = Template(miHtml1.read())
+    
+    miHtml1.close()
+    
+    miContexto = context(miContexto)
+    
+    documento = plantilla.render(miContexto)
+    
+    return HttpResponse(documento)
+    
+    
+    
